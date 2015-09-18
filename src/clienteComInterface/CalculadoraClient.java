0056adb262cb279cbb4ae61830dda09e3663cb73
will <professorwillians@gmail.com>
2015-09-18 18:55:44 -0300

    comite atomico - gacp

---
+++ src/clienteComInterface/CalculadoraClient.java
@@ -0,0 +1,24 @@
+package clienteComInterface;
+ 
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+
+import wsServer.CalculadoraServer;
+ 
+public class CalculadoraClient {
+ 
+    public static void main(String args[]) throws Exception {
+        URL url = new URL("http://127.0.0.1:9876/calc?wsdl");
+        QName qname = new QName("http://ws/","CalculadoraServerImplService");
+        Service ws = Service.create(url, qname);
+        CalculadoraServer calc = ws.getPort(CalculadoraServer.class);
+ 
+        System.out.println("Soma (5+1): " + calc.soma(5,1));
+        System.out.println("Subtracao (5-1): " + calc.subtracao(5,1));
+        System.out.println("Multiplicacao (5*1): " + calc.multiplicacao(5,1));
+        System.out.println("Divisao (5/1): " + calc.divisao(5,1));
+ 
+    }
+}
