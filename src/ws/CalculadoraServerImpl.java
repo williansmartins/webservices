c86b3624fae09d15545190c57ab6b0d6793393fd
will <professorwillians@gmail.com>
2015-09-18 18:15:02 -0300

    comite atomico - gacp

---
+++ src/ws/CalculadoraServerImpl.java
@@ -0,0 +1,24 @@
+package ws;
+import javax.jws.WebService;
+
+
+@WebService(endpointInterface = "ws.CalculadoraServer")
+public class CalculadoraServerImpl implements CalculadoraServer {
+         
+    public float soma(float num1, float num2) {
+        return num1 + num2;
+    }
+     
+    public float subtracao(float num1, float num2) {
+        return num1 - num2;
+    }
+ 
+    public float multiplicacao(float num1, float num2) {
+        return num1 * num2;
+    }
+ 
+    public float divisao(float num1, float num2) {
+        return num1 / num2;
+    }
+ 
+}
